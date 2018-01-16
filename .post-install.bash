c952406269f83a374373889987b03969b381293a
Beaglefoot <ch-e-st@yandex.ru>
2018-01-16 18:46:15 +0300

    Update post-install

--- .post-install.bash
+++ .post-install.bash
@@ -3,7 +3,7 @@
 apt-get update
 curl -sL https://deb.nodesource.com/setup_8.x | bash -
 apt-get install -y nodejs
-npm install -g babel-eslint dumper eslint eslint-plugin-import eslint-plugin-react gulp js-beautify webpack webpack-dev-server yarn
+npm install -g babel-eslint dumper eslint eslint-plugin-import eslint-plugin-react gulp js-beautify webpack webpack-dev-server yarn nodemon
 apt-get install -y vim vim-gtk git tmux
 apt-get autoremove -y --purge
 git clone --depth 1 --branch release https://github.com/adobe-fonts/source-code-pro.git /usr/share/fonts/SourceCodePro
