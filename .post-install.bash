e93d44b5f3660fbe7490f4e82c061c5d22a4e449
Beaglefoot <ch-e-st@yandex.ru>
2018-01-16 17:22:37 +0300

    Add bashrc and post-install script for ubuntu

---
+++ .post-install.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+apt-get update
+curl -sL https://deb.nodesource.com/setup_8.x | bash -
+apt-get install -y nodejs
+npm install -g babel-eslint dumper eslint eslint-plugin-import eslint-plugin-react gulp js-beautify webpack webpack-dev-server yarn
+apt-get install -y vim vim-gtk git tmux
+apt-get autoremove -y --purge
+git clone --depth 1 --branch release https://github.com/adobe-fonts/source-code-pro.git /usr/share/fonts/SourceCodePro
+fc-cache -f
+cp /usr/lib/git-core/git-sh-prompt ~/.git-prompt.sh
