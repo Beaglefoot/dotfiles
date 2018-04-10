eeb8bb343d91575fdf8b6dd9a8d764194040e241
Beaglefoot <ch-e-st@yandex.ru>
2018-04-10 19:47:01 +0300

    Update .post-install.bash

--- .post-install.bash
+++ .post-install.bash
@@ -3,11 +3,12 @@
 curl -sL https://deb.nodesource.com/setup_8.x | bash -
 
 apt-get update
-apt-get install -y nodejs vim vim-gtk git tmux ubuntu-make gpick shutter
+apt-get install -y nodejs vim vim-gtk git tmux gpick shutter
 
-npm install -g babel-eslint dumper eslint eslint-plugin-import eslint-plugin-react gulp js-beautify webpack webpack-dev-server yarn nodemon browser-sync
+npm install -g dumper js-beautify webpack webpack-dev-server yarn nodemon browser-sync serve
 
 apt-get autoremove -y --purge
 
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone --depth 1 --branch release https://github.com/adobe-fonts/source-code-pro.git /usr/share/fonts/SourceCodePro
 fc-cache -f
