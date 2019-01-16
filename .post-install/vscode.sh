a0b25f280bf1e5cb8906e7f6a6bdedf1e48657e9
Stanislav Chernov <schernov@pushwoosh.com>
2019-01-16 10:45:43 +0300

    Link vscode config on mac os

--- .post-install/vscode.sh
+++ .post-install/vscode.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-while read ext; do
-    code --install-extension $ext
-done < vscode-extensions-list.txt
+# while read ext; do
+#     code --install-extension $ext
+# done < vscode-extensions-list.txt
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    filename="$HOME/Library/Application Support/Code/User/settings.json"
+    rm "$filename"
+    ln -s "$HOME/.config/Code/User/settings.json" "$filename"
+fi
